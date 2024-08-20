07db0ec6703262af1cdbaf1042446ae8d0ce0683
Fabian Beutel <fbeutel@users.noreply.github.com>
2024-08-20 15:33:18 +0200

    Relay: don't panic in case of error

--- src/bridge.rs
+++ src/bridge.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use futures::{SinkExt, StreamExt};
+use tokio::task::JoinHandle;
 
 use crate::{protocol::ServerPath, util::build_request};
 
@@ -19,25 +20,29 @@ pub async fn bridge(server_addr: String, peer_name: String, exposed: String) -> 
     let (mut ws_server_out, mut ws_server_in) = ws_server_stream.split();
     let (mut ws_exposer_out, mut ws_exposer_in) = ws_exposer_stream.split();
 
-    let task_1 = tokio::spawn(async move {
+    let task_1: JoinHandle<Result<()>> = tokio::spawn(async move {
         while let Some(msg) = ws_server_in.next().await {
-            let msg = msg.expect("Failed to get message");
+            let msg = msg?;
             log::debug!("server -> exposer: {} bytes", msg.len());
-            ws_exposer_out.send(msg).await.expect("Failed to send to exposer");
+            ws_exposer_out.send(msg).await?;
         }
+
+        Ok(())
     });
 
-    let task_2 = tokio::spawn(async move {
+    let task_2: JoinHandle<Result<()>> = tokio::spawn(async move {
         while let Some(msg) = ws_exposer_in.next().await {
-            let msg = msg.expect("Failed to get message");
+            let msg = msg?;
             log::debug!("exposer -> server: {} bytes", msg.len());
-            ws_server_out.send(msg).await.expect("Failed to send to server");
+            ws_server_out.send(msg).await?;
         }
+
+        Ok(())
     });
 
     // join tasks
-    task_1.await?;
-    task_2.await?;
+    task_1.await??;
+    task_2.await??;
 
     Ok(())
 }
