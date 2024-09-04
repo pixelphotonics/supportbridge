492c1059e51fc128da6a703ff403cd4c0e1a34d8
Fabian Beutel <fbeutel@users.noreply.github.com>
2024-09-04 09:17:20 +0200

    Streamline the expose implementation, send 'ack_init' message upon receiving an 'init' message

--- src/expose.rs
+++ src/expose.rs
@@ -5,112 +5,81 @@ use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::oneshot;
 use tokio::sync::Mutex;
 
-pub struct ClientConnection {
-    pub write_half: tokio::net::tcp::OwnedWriteHalf,
-    pub close_sender: oneshot::Sender<()>,
-}
+use crate::util::{spawn_guarded, GuardedJoinHandle};
+
 
 async fn handle_connection(listen_stream: TcpStream, target_addr: SocketAddr) -> Result<()> {
     let ws_stream = tokio_tungstenite::accept_async(listen_stream).await?;
-    let (mut ws_out, ws_in) = ws_stream.split();
+    let (ws_out, mut ws_in) = ws_stream.split();
 
-    let (ws_send_sender, mut ws_send_receiver) = tokio::sync::mpsc::channel::<Vec<u8>>(1);
+    let ws_out_mut = Arc::new(Mutex::new(ws_out));
 
-    // Handle messages on ws_send_receiver
-    tokio::spawn(async move {
-        while let Some(msg) = ws_send_receiver.recv().await {
-            if let Err(e) = ws_out.send(tungstenite::Message::Binary(msg)).await {
-                log::error!("Error sending to ws: {:?}", e);
-                break;
-            }
-        }
-    });
+    let target_write: Arc<Mutex<Option<tokio::net::tcp::OwnedWriteHalf>>> = Arc::new(Mutex::new(None));
+    let mut task_tcp_to_ws: Option<GuardedJoinHandle<Result<()>>> = None;
 
-    // Read from the websocket and write to the target socket
-    tokio::spawn(async move {
-        let target_socket: Arc<Mutex<Option<ClientConnection>>> = Arc::new(Mutex::new(None));
+    while let Some(msg) = ws_in.next().await {
+        let msg = msg?;
+        match msg {
+            tungstenite::Message::Text(textmsg) => {
+                match textmsg.as_str() {
+                    "init" => {
+                        // Cancel old task by clearing the task variable
+                        if let Some(task) = task_tcp_to_ws.take() {
+                            task.abort();
+                        }
 
-        ws_in
-            .for_each(|msg| async {
-                if msg.is_err() {
-                    log::error!("Error reading from ws: {:?}", msg.err().unwrap());
-                    return;
-                }
+                        // Acquire the ws_out lock to ensure that no more messages can be sent to the websocket from any old send task
+                        let mut ws_out_lock = ws_out_mut.clone().lock_owned().await;
 
-                let msg = msg.unwrap();
+                        // Send ack_init to the connected client to mark that from this point on no more data from an old connection will be sent
+                        ws_out_lock.send(tungstenite::Message::Text("ack_init".to_string())).await?;
 
-                if msg.is_text() {
-                    match msg.to_text().unwrap() {
-                        "init" => {
-                            let target_socket = target_socket.clone();
-                            let mut socket_lock = target_socket.lock().await;
+                        let mut tcp_out_lock = target_write.clone().lock_owned().await;
 
-                            // Create new socket connection
-                            let new_socket = TcpStream::connect(target_addr).await.unwrap();
-                            debug!("Open socket to target: {}", target_addr);
-                            let (mut target_read, target_write) = new_socket.into_split();
+                        // Create new socket connection
+                        let new_socket = TcpStream::connect(target_addr).await.unwrap();
+                        debug!("Open socket to target: {}", target_addr);
+                        let (mut target_read, target_write_half) = new_socket.into_split();
+                        tcp_out_lock.replace(target_write_half);
 
-                            let (close_sender, mut close_receiver) = oneshot::channel();
-                            
-                            let old_connection = socket_lock.replace(ClientConnection{
-                                write_half: target_write,
-                                close_sender,
-                            });
-
-                            if let Some(old_connection) = old_connection {
-                                debug!("Close existing target port");
-                                _ = old_connection.close_sender.send(());
+                        task_tcp_to_ws = Some(spawn_guarded(async move {
+                            loop {
+                                let mut buf = vec![0; 1024];
+                                let n = target_read.read(buf.as_mut_slice()).await?;
+                                if n == 0 {
+                                    break;
+                                }
+                    
+                                log::debug!("TCP->WS: {} bytes", n);
+                                ws_out_lock
+                                    .send(tungstenite::Message::Binary(buf[..n].to_vec()))
+                                    .await?;
                             }
 
-                            let wssender = ws_send_sender.clone();
-
-                            tokio::spawn(async move {
-                                loop {
-                                    let mut buf = vec![0; 1024];
-                                    // select on target_read and close_receiver
-                                    tokio::select! {
-                                        read_result = target_read.read(&mut buf) => {
-                                            match read_result {
-                                                Ok(n) => {
-                                                    if n == 0 {
-                                                        break;
-                                                    }
-
-                                                    wssender.send(buf[..n].to_vec()).await.expect("Failed to send to ws");
-                                                }
-                                                Err(e) => {
-                                                    debug!("Error reading from target: {:?}", e);
-                                                    break;
-                                                }
-                                            
-                                            }
-                                        }
-                                        _ = &mut close_receiver => {
-                                            debug!("Closing connection");
-                                        }
-                                    }
-                                }
-                            });
-                        },
-                        _ => {
-                            log::warn!("Unknown message: {}", msg.to_text().unwrap());
-                        }
-                    }
-                } else if msg.is_binary() {
-                    // forward to open socket, if any
-                    if let Some(socket) = target_socket.lock().await.as_mut() {
-                        log::debug!("Forwarding message to target, {} bytes", msg.len());
-                        socket.write_half.write(&msg.into_data()).await.expect("Failed to write to target");
-                    } else {
-                        log::warn!("No target socket available. Init message required.");
+                            Ok(())
+                        }));
+                    },
+                    _ => {
+                        log::warn!("Unknown message: {}",textmsg);
                     }
                 }
-            })
-            .await;
-    });
+            },
+            tungstenite::Message::Binary(binmsg) => {
+                if let Some(target_write) = target_write.lock().await.as_mut() {
+                    log::debug!("Forwarding message to target, {} bytes", binmsg.len());
+                    target_write.write(&binmsg).await.expect("Failed to write to target");
+                } else {
+                    log::warn!("No target socket available. Init message required.");
+                }
+            },
+            _ => {
+                log::debug!("Unknown message: {:?}", msg);
+            }
+        }
+    }
+
 
     Ok(())
 }
